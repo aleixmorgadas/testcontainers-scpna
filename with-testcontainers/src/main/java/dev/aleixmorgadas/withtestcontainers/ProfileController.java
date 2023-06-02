4d414ec18814a740be3918c79ba36ed57a456d27
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-06-02 16:37:10 +0200

    ProfileController

---
+++ with-testcontainers/src/main/java/dev/aleixmorgadas/withtestcontainers/ProfileController.java
@@ -0,0 +1,39 @@
+package dev.aleixmorgadas.withtestcontainers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(ProfileController.URI)
+public class ProfileController {
+    public static final String URI = "/profile";
+
+    @Autowired
+    ProfileRepository repository;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping()
+    ResponseEntity<List<Profile>> all() {
+        kafkaTemplate.send("request", "all");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/insert")
+    ResponseEntity<Profile> save() {
+        kafkaTemplate.send("request", "save");
+        var profile = new Profile(UUID.randomUUID().toString(), "Luis");
+        repository.save(profile);
+        return ResponseEntity.ok(profile);
+    }
+}
