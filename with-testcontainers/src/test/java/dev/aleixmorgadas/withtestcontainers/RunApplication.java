71e9ec8f3b38b01f7cff6796d15e8177ae67817c
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-06-02 16:37:59 +0200

    RunApplication

---
+++ with-testcontainers/src/test/java/dev/aleixmorgadas/withtestcontainers/RunApplication.java
@@ -0,0 +1,39 @@
+package dev.aleixmorgadas.withtestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class RunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(WithTestcontainersApplication::main)
+                .with(ContainerConfiguration.class)
+                .run(args);
+    }
+
+    @TestConfiguration
+    static class ContainerConfiguration {
+
+        @Bean()
+        @RestartScope()
+        @ServiceConnection
+        KafkaContainer kafkaContainer() {
+            return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.3.0"))
+                    .withReuse(true);
+        }
+
+        @Bean
+        @RestartScope
+        @ServiceConnection
+        PostgreSQLContainer<?> postgreSQLContainer() {
+            return new PostgreSQLContainer<>("postgres:15.1-alpine")
+                    .withReuse(true);
+        }
+    }
+}
